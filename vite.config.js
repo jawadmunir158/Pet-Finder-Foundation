ba2c4921f3ca3eafc3e1ab97e81d54f9ac681f7b
jadi <jawadmunir158@gmail.com>
2024-10-18 21:27:27 +0500

    first change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Pet-Finder-Foundation/', // Ensure this points to your repository name
   plugins: [react()],
 })
